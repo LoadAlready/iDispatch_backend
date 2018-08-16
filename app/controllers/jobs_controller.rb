387bcc4aaf25bb762584be011defb61e7aed5d6c
loadalready <justaviram@gmail.com>
2018-08-16 15:38:05 -0400

    working create action in job controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,8 +5,21 @@ class JobsController < ApplicationController
   end
 
   def create
-    @job = Job.new
-    byebug
+    @job = Job.new(client_id: params[:job][:client_id], 
+                    job_number: params[:job][:job_number], 
+                    refrence: params[:job][:refrence], 
+                    schedule_date_month: params[:job][:schedule_date_month], 
+                    schedule_date_day: params[:job][:schedule_date_day], 
+                    schedule_date_year: params[:job][:schedule_date_year], 
+                    schedule_time: params[:job][:schedule_time], 
+                    time_arrived: params[:job][:time_arrived], 
+                    time_completed: params[:job][:time_arrived],
+                    job_notes: params[:job][:job_notes],
+                    description: params[:job][:description])
+    @job.save
+
+    @crew = Crew.new(user_id: params[:job][:leadTechID], job_id: @job.id )
+    @crew.save
   end
 
   def show
@@ -17,6 +30,6 @@ class JobsController < ApplicationController
   private
  
   def job_params
-    params.permit!
+    params.require(:job).permit!
   end
 end
