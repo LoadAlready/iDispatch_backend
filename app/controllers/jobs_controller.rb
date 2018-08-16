bc679f299c6f2a78693a6a85085d9113d4429e31
loadalready <justaviram@gmail.com>
2018-08-16 14:15:38 -0400

    working on bug with create action in Job controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,8 +4,19 @@ class JobsController < ApplicationController
     render json: @jobs
   end
 
+  def create
+    @job = Job.new
+    byebug
+  end
+
   def show
     @job = Job.all.find(params[:id])
     render json: @job
+  end
+
+  private
+ 
+  def job_params
+    params.permit!
   end
 end
